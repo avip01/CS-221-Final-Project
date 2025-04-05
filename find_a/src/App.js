05cced52d455bba43b67274896398b3bf279afd8
Marion Office <87404335+Marioffic@users.noreply.github.com>
2025-04-05 15:03:03 -0500

    added basic routing and unstyled nav-bar

--- find_a/src/App.js
+++ find_a/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
 import AboutUs from './aboutMe';
+import Landing from './landing';
+import './App.css';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-      <div className="App">
-        <AboutUs />
-      </div>
+        <div className="App">
+          <Navbar /> 
+
+          <main className="content">
+            <Routes> {/* Define routes */}
+              {/* Route for the landing page */}
+              <Route path="/" element={<Landing />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/landing" element={<Landing />} />
+              // routes to othe pages go here
+            </Routes>
+          </main>
+        </div>
   );
 }
 
---
+++ find_a/src/landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div>
+            hey
+        </div>
+    );
+}
+
+export default Landing;
