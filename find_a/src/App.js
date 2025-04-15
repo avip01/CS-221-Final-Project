0c7c902210cef64c777f90edd37171b5038503a1
Nehemiah_Sanders <nehitimothy10@gmail.com>
2025-04-15 16:54:52 -0500

    edited warnings from code analysis

--- find_a/src/App.js
+++ find_a/src/App.js
@@ -1,11 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './landing';
-import Login from './login';
-import SignUp from './signUp';
-import Account from './account';
-import AboutUs from './aboutUs';
-import Settings from './settings';
-
 function App() {
 
 }
